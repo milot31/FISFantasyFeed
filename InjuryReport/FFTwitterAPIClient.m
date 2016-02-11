b360c19cd51736e4a3093d73f9dad3605a637e51
milot31 <milot.phil@gmail.com>
2016-02-10 20:06:43 -0500

    branch

--- InjuryReport/FFTwitterAPIClient.m
+++ InjuryReport/FFTwitterAPIClient.m
@@ -12,23 +12,23 @@
 
 +(void)getTwitterStatuses: (NSString *)playerName withCompletion: (void (^) (NSArray* statuses))completionBlock{
     
-STTwitterAPI *twitter = [STTwitterAPI twitterAPIAppOnlyWithConsumerKey:consumerKey
-                                                        consumerSecret:consumerSecret];
+    STTwitterAPI *twitter = [STTwitterAPI twitterAPIAppOnlyWithConsumerKey:consumerKey
+                                                            consumerSecret:consumerSecret];
 
-[twitter verifyCredentialsWithUserSuccessBlock:^(NSString *username, NSString *userID) {
+    [twitter verifyCredentialsWithUserSuccessBlock:^(NSString *username, NSString *userID) {
 
-    NSString *searchQuery = [NSString stringWithFormat:@"(from:Stephania_ESPN OR from:MatthewBerryTMR OR from:thepme OR from:Michael_Fabiano OR from:YahooFootball OR from:evansilva OR from:scotteRotoEx OR from:josinaanderson OR from:allinkid OR from:EricMackFantasy OR from:nfl OR from:espn OR from:sportscenter) AND %@ since:2015-09-01", playerName];
-    
-    [twitter getSearchTweetsWithQuery:searchQuery successBlock:^(NSDictionary *searchMetadata, NSArray *statuses) {
-        completionBlock(statuses);
+        NSString *searchQuery = [NSString stringWithFormat:@"(from:Stephania_ESPN OR from:MatthewBerryTMR OR from:thepme OR from:Michael_Fabiano OR from:YahooFootball OR from:evansilva OR from:scotteRotoEx OR from:josinaanderson OR from:allinkid OR from:EricMackFantasy OR from:nfl OR from:espn OR from:sportscenter) AND %@ since:2015-09-01", playerName];
+        
+        [twitter getSearchTweetsWithQuery:searchQuery successBlock:^(NSDictionary *searchMetadata, NSArray *statuses) {
+            completionBlock(statuses);
+        } errorBlock:^(NSError *error) {
+            NSLog(@"%@", error);
+        }];
+        
     } errorBlock:^(NSError *error) {
-        NSLog(@"%@", error);
+        NSLog(@"-- %@", [error localizedDescription]);
+        
     }];
-    
-} errorBlock:^(NSError *error) {
-    NSLog(@"-- %@", [error localizedDescription]);
-    
-}];
 }
 
 
