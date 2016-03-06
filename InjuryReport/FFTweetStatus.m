eecf6ce356e708992ea50f4d952a1549ebd1ddd3
milot31 <milot.phil@gmail.com>
2016-03-06 18:15:03 -0500

    updated date display on twitter feed

--- InjuryReport/FFTweetStatus.m
+++ InjuryReport/FFTweetStatus.m
@@ -16,9 +16,7 @@
     if (dictionary[@"retweeted_status"] != nil) {
         someStatus.isRetweet = YES;
         someStatus.retweetHandle = dictionary[@"retweeted_status"][@"user"][@"name"];
-        NSLog(@"%@", someStatus.retweetHandle);
         someStatus.tweetText = dictionary[@"retweeted_status"][@"text"];
-        NSLog(@"%@", someStatus.tweetText);
     } else {
         someStatus.isRetweet = NO;
         someStatus.retweetHandle = nil;
@@ -29,13 +27,63 @@
     someStatus.realName = [dictionary valueForKeyPath:@"user.name"];
     someStatus.twitterHandle = [NSString stringWithFormat:@"@%@", [dictionary valueForKeyPath:@"user.screen_name"]];
     someStatus.imageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:[dictionary valueForKeyPath:@"user.profile_image_url_https"]]];
-    NSDateFormatter * dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"EEE MMM dd HH:mm:ss ZZZ yyyy"];
-    NSDate* createdDate = [dateFormatter dateFromString:dictionary[@"created_at"]];
-    NSDateFormatter *visibleDate = [[NSDateFormatter alloc]init];
-    [visibleDate setDateFormat:@"MMM dd ''yy',' h:mm a"];
-    someStatus.createdDate = [visibleDate stringFromDate:createdDate];
+    
+//    NSDateFormatter * dateFormatter = [[NSDateFormatter alloc] init];
+//    [dateFormatter setDateFormat:@"EEE MMM dd HH:mm:ss ZZZ yyyy"];
+//    NSDate* createdDate = [dateFormatter dateFromString:dictionary[@"created_at"]];
+//    NSDateFormatter *visibleDate = [[NSDateFormatter alloc]init];
+//    [visibleDate setDateFormat:@"MMM dd ''yy',' h:mm a"];
+//    someStatus.createdDate = [visibleDate stringFromDate:createdDate];
+    
+    NSString *date = dictionary[@"created_at"];
+    someStatus.createdDate = [self getTimeSincePost:date];
+    
     return someStatus;
+}
+
++(NSString *)getTimeSincePost:(NSString *)dateString {
+    
+    //date from twitter JSON
+    NSDateFormatter *twitterFormatter = [[NSDateFormatter alloc]init];
+    [twitterFormatter setDateFormat:@"EEE MMM dd HH:mm:ss ZZZ yyyy"];
+    NSDate *createdDate = [twitterFormatter dateFromString:dateString];
+    
+    NSDateFormatter *properFormat = [[NSDateFormatter alloc]init];
+    [properFormat setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString *stringFromDate = [properFormat stringFromDate:createdDate];
+
+    //current date
+    NSDateFormatter *currentFormat = [[NSDateFormatter alloc]init];
+    [currentFormat setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString *currentDate = [currentFormat stringFromDate:[NSDate date]];
+    
+    
+    NSDateFormatter *labelFormat = [[NSDateFormatter alloc]init];
+    [labelFormat setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSDate *start = [labelFormat dateFromString:stringFromDate];
+    NSDate *end = [labelFormat dateFromString:currentDate];
+    NSCalendar *calendar = [[NSCalendar alloc]initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *dayComps = [calendar components:NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond fromDate:start toDate:end options:NSCalendarWrapComponents];
+    
+    NSInteger daysSince = [dayComps day];
+    NSInteger hoursSince = [dayComps hour];
+    NSInteger minutesSince = [dayComps minute];
+    NSInteger secondsSince = [dayComps second];
+    
+    NSString *timeSince = @"";
+    
+    if (daysSince >= 1) {
+        timeSince = [NSString stringWithFormat:@"%lid ago", daysSince];
+    } else if (daysSince < 1 && hoursSince >= 1) {
+        timeSince = [NSString stringWithFormat:@"%lih ago", hoursSince];
+    } else if (hoursSince < 1 && minutesSince >= 1) {
+        timeSince = [NSString stringWithFormat:@"%lim ago", minutesSince];
+    } else if (minutesSince < 1) {
+        timeSince = [NSString stringWithFormat:@"%lis ago", secondsSince];
+    }
+    
+    return timeSince;
+    
 }
 
 @end
